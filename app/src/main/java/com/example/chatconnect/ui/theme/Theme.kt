a3c0e6edf19bad6b292d72b6625617a1c045231d
Anurag Mishra <anurag026mishra@gmail.com>
2023-11-06 17:09:24 +0530

    chatConnect

---
+++ app/src/main/java/com/example/chatconnect/ui/theme/Theme.kt
@@ -0,0 +1,35 @@
+package com.example.chatconnect.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+
+private val DarkColorPalette = darkColorScheme(
+    primary = Purple200,
+    //primaryVariant = Purple700,
+    secondary = Teal200
+)
+
+private val LightColorPalette = lightColorScheme(
+    primary = Purple500,
+    //primaryVariant = Purple700,
+    secondary = Teal200
+)
+
+@Composable
+fun FlashChatTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+        colorScheme=  colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
